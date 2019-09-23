7d9dc6b45b93a33fc9974ac88e71d748812db3f9
positiveWand <singbhang@gmail.com>
2019-09-23 20:58:46 +0900

    3주차 과제 보충

--- week3/assignment1.cpp
+++ week3/assignment1.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int data = 9;
-static int bss;
-char* rodata = "Hello World!";
+int data = 9; //0이 아닌 값으로 초기화된 변수를 data 영역에 저장
+static int bss; //초기화되지 않은 변수, 정적 변수를 저장
+char* rodata = "Hello World!"; //변경되지 않는 상수(문자, 숫자) 발생지
 
 int main() {
-	int* code = (int*) &main;
-	int* heap = new int[1];
-	int stack = 1;
+	int* code = (int*) &main; //코드가 있는 영역(함수도 포인터가 있음)
+	int* heap = new int[1]; //동적 할당 되는 변수들이 저장되는 공간
+	int stack = 1; //함수 호출 시(인스턴스) 사용되는 영역
 
 	std::cout << "code\t" << (void*) code <<std::endl;
 	std::cout << "Rodata\t" << (void*) rodata << std::endl;
--- week3/assignment3.cpp
+++ week3/assignment3.cpp
@@ -9,4 +9,9 @@ int main() {
 	char * copy = "is no no";
 	char name = 'a';
 	std::cout << &name+i << std::endl;
+	//std::cout << &copy << std::endl;
+	//std::cout << &c << std::endl;
+	//std::cout << &i << std::endl;
+	//std::cout << &a << std::endl;
+	//std::cout << &b << std::endl;
 }
